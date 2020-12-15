d513047c28330450a267ca18ab8d54feaea2a6d3
Miriam-art <gulshat.saparova@gmail.com>
2020-12-14 21:47:15 -0600

    Gulshat SDMA4 test files added

--- src/main/java/PageFactory/GulshatPage.java
+++ src/main/java/PageFactory/GulshatPage.java
@@ -22,4 +22,52 @@ public class GulshatPage {
     @FindBy(how = How.XPATH, using = "//button[text()='New Hire']")
     public static WebElement newHireButton;
 
+//Details section
+    @FindBy(xpath = "//a[contains(text(),'Details')]")
+    public static WebElement details;
+
+    @FindBy(xpath = "//label[normalize-space()='Original Hire Date:']")
+    public static WebElement originalHireDate;
+
+    @FindBy(xpath = "//label[contains(text(),'Contract Work Days:')]")
+    public static WebElement contractWorkDays;
+
+    @FindBy(xpath = "//label[contains(text(),'Employment Basis:')]")
+    public static WebElement employmentBasis;
+
+    @FindBy(xpath = "//label[contains(text(),'Education Level:')]")
+    public static WebElement educationalLevel;
+
+    @FindBy(xpath = "//label[contains(text(),'Annual Contract Work Months:')]")
+    public static WebElement annualContractWorkMonth;
+
+    @FindBy(xpath = "//label[contains(text(),'Previous Years of Teaching Experience:')]")
+    public static WebElement previousYearsOfTeachingExperience;
+
+    @FindBy(xpath = "//label[contains(text(),'Birth Date:')]")
+    public static WebElement birthDate;
+
+    @FindBy(xpath = "//label[contains(text(),'Gender:')]")
+    public static WebElement gender;
+
+    @FindBy(xpath = "//label[contains(text(),'Native Language:')]")
+    public static WebElement nativeLanguage;
+
+    @FindBy(xpath = "//label[contains(text(),'Secondary Language:')]")
+    public static WebElement secondaryLanguage;
+
+    @FindBy(xpath = "//label[contains(text(),'Place of Birth:')]")
+    public static WebElement placeOfBirth;
+
+    @FindBy(xpath = "//label[contains(text(),'Country of Origin:')]")
+    public static WebElement countryOfOrigin;
+
+    @FindBy(xpath = "//label[contains(text(),'Hispanic:')]")
+    public static WebElement hispanic;
+
+    @FindBy(xpath = "//label[contains(text(),'Race:')]")
+    public static WebElement race;
+
+//    @FindBy(xpath = "//a[contains(text(),'Derek Parsons')]")
+//    public static WebElement person;
 }
--- src/test/java/stepDefs/GulshatStepDef.java
+++ src/test/java/stepDefs/GulshatStepDef.java
@@ -9,21 +9,23 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GulshatStepDef {
+    WebDriver driver = Driver.getDriver();
 
     @Given("I logged in Gulshat")
-    public void iLoggedInGulshat() throws Exception{
+    public void iLoggedInGulshat() throws Exception {
         PageObjectMgr.setCurrentPage("GulshatPage");
-        WebElement loadLogin=(WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(),"loadLogin");
-        WebElement emailTextBox=(WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(),"emailSection");
-        WebElement passwordTextBox=(WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(),"passwordSection");
-        WebElement loginButton=(WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(),"loginButton");
-        WebElement newHire=(WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(),"newHireButton");
+        WebElement loadLogin = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "loadLogin");
+        WebElement emailTextBox = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "emailSection");
+        WebElement passwordTextBox = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "passwordSection");
+        WebElement loginButton = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "loginButton");
+        WebElement newHire = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "newHireButton");
 
         loadLogin.click();
         Thread.sleep(1000);
@@ -50,9 +52,39 @@ public class GulshatStepDef {
     }
 
     @Then("I verify value is as expected")
-    public void iVerifyValueIsAsExpected() {
-        String actual=GulshatPage.newHireButton.getText();
-        String expected="value";
-        Assert.assertEquals(expected,actual);
+    public void iVerifyValueIsAsExpected() throws Exception {
+        try {
+            WebElement person = driver.findElement(By.xpath("//a[contains(text(),'Derek Parsons')]"));
+            String actual = driver.findElement(By.xpath("//div[contains(text(),'08/15/2017')]")).getText();
+            String expected = "08/15/2017";
+            Assert.assertEquals(expected, actual);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //throw new Exception("try again");
+        }
+
+
+    }
+
+    @When("I click on {string} tab as Gulshat")
+    public void iClickOnTabAsGulshat(String elementNm) {
+        WebElement element = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), elementNm);
+        element.click();
+
+    }
+
+    @Then("I verified that {string} element is exist with {string} text")
+    public void iVerifiedThatElementIsExistWithText(String elementNm, String expected) throws Exception {
+        WebElement fields = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), elementNm);
+        String label = fields.getText();
+        System.out.println(elementNm);
+        System.out.println(expected);
+        if (!expected.equalsIgnoreCase(label)) {
+            throw new Exception("field name does not match");
+        }
+
+
     }
 }
+
+
