fdaea09e7ffebefba375acbfc61bf18db12c933a
Miriam-art <gulshat.saparova@gmail.com>
2021-01-01 15:29:35 -0600

    Runner file corrected

--- src/test/java/stepDefs/GulshatStepDef.java
+++ src/test/java/stepDefs/GulshatStepDef.java
@@ -13,7 +13,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
 
 public class GulshatStepDef {
     WebDriver driver = Driver.getDriver();
@@ -39,10 +43,6 @@ public class GulshatStepDef {
 
     }
 
-    @When("I run the test")
-    public void iRunTheTest() {
-
-    }
 
     @When("I click on {string} as Gulshat")
     public void iClickOnAsGulshat(String element) {
@@ -51,20 +51,18 @@ public class GulshatStepDef {
                 6).until(ExpectedConditions.visibilityOf(GulshatPage.newHireButton));
     }
 
-    @Then("I verify value is as expected")
-    public void iVerifyValueIsAsExpected() throws Exception {
-        try {
-            WebElement person = driver.findElement(By.xpath("//a[contains(text(),'Derek Parsons')]"));
-            String actual = driver.findElement(By.xpath("//div[contains(text(),'08/15/2017')]")).getText();
-            String expected = "08/15/2017";
-            Assert.assertEquals(expected, actual);
-        } catch (Exception e) {
-            e.printStackTrace();
-            //throw new Exception("try again");
-        }
-
-
-    }
+//    @Then("I verify value is as expected")
+//    public void iVerifyValueIsAsExpected() throws Exception {
+//        try {
+//            WebElement person = driver.findElement(By.xpath("//a[contains(text(),'Derek Parsons')]"));
+//            String actual = driver.findElement(By.xpath("//div[contains(text(),'08/15/2017')]")).getText();
+//            String expected = "08/15/2017";
+//            Assert.assertEquals(expected, actual);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            //throw new Exception("try again");
+//        }
+//    }
 
     @When("I click on {string} tab as Gulshat")
     public void iClickOnTabAsGulshat(String elementNm) {
@@ -82,7 +80,34 @@ public class GulshatStepDef {
         if (!expected.equalsIgnoreCase(label)) {
             throw new Exception("field name does not match");
         }
+
+    }
+
+
+    @Then("I validate position filter is working")
+    public void iValidatePositionFilterIsWorking() throws InterruptedException {
+        String expected = "";
+        String actual = "";
+        List<WebElement> positions = (List<WebElement>) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "positions");
+        List<WebElement> staffMembers = (List<WebElement>) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "staffMembers");
+        WebElement position = (WebElement) WebElementMgr.getWebElement(PageObjectMgr.getCurrentPage(), "position");
+        for (int i = 1; i < positions.size(); i++) {
+            positions.get(i).click();
+            expected = positions.get(i).getText();
+
+            for (int j = 2; j < staffMembers.size(); j++) {
+                staffMembers.get(j).click();
+                Thread.sleep(1000);
+                actual = position.getText();
+                System.out.println(expected + " as expected  " + actual + "   Actual");
+                Assert.assertEquals("position does not match with positions filter",expected,actual);
+            }
+
+
+        }
+
     }
 }
+
 
 
