5154ddf318a3545e5c0f8b4876ac1a44eb1cc23b
Miriam-art <gulshat.saparova@gmail.com>
2021-01-25 17:09:07 -0600

    fix

--- src/test/java/testRunners/Runner.java
+++ src/test/java/testRunners/Runner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"pretty", "json:target/cucumber.json",
                 "html:target/cucumber-html-reports"}
 
-        ,tags = {"@Gulshat"}
+        ,tags = {"@SDMA1"}
 
 
 )
