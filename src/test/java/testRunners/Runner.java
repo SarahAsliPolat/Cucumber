9b3e3353e3dc1596622daffe21592827c5da0249
ismail002 <adjissiismail6@gmail.com>
2021-02-24 17:37:11 -0600

    changes reverted on the comon files

--- src/test/java/testRunners/Runner.java
+++ src/test/java/testRunners/Runner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"pretty", "json:target/cucumber.json",
                 "html:target/cucumber-html-reports"}
 
-        ,tags = {"@ismail"}
+        ,tags = {"@SDMA1"}
 
 
 )
